002fd0622a9f4739d633d0cd836323e909c2e8c3
VikasDewangan <dewanganvikas192@gmail.com>
2025-05-14 12:51:36 +0530

    navbar is complete for now.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,10 +11,10 @@ const Navbar = () => {
   return (
     <nav
       className={`
-          ${styles.paddingX} w-full flex py-5 fixed top-0 z-20 bg-black items-center 
+          ${styles.paddingX} w-full flex fixed top-0 z-20 bg-black items-center 
           `}
     >
-      <div className="w-full flex justify-between items-center max-w-7xl h-20 mx-auto">
+      <div className="w-full flex   justify-between items-center max-w-7xl h-20  mx-auto">
         <Link
           to="/"
           className="flex items-center gap-2"
@@ -25,7 +25,7 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex items-center">
-            Vikas &nbsp;<span className="sm:block hidden">Dewangan</span>
+            Vikas &nbsp; <span className="sm:block  ">Dewangan</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
@@ -42,7 +42,7 @@ const Navbar = () => {
               <Link to={`#${link.id}`}>{link.title}</Link>
             </li>
           ))}
-          <p
+          <li
             onClick={() => {
               window.open(
                 "https://drive.google.com/file/d/1HAnzHmD9-tC7LUO_SWOalmwINAWB0Bbi/view?usp=sharing"
@@ -52,18 +52,51 @@ const Navbar = () => {
           >
             {" "}
             Resume
-          </p>
+          </li>
         </ul>
 
         <div className="sm:hidden flex flex-1 justify-end items-center">
           <img
-            src={toggle ? close : menu}
+            src={toggle ? menu : close}
             alt="menu"
             className="w-[28px] h-[28px] object-contain cursor-pointer"
             onClick={() => {
               setToggle(!toggle);
             }}
           />
+          <div
+            className={`${
+              toggle ? "hidden " : "flex"
+            } p-4 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className="list-none  flex justiyfy-end items-center flex-col gap-4 p-2">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-gray-500"
+                  } hover:text-white text-[18px] font-medium cursor-pointer`}
+                  onClick={() => {
+                    setToggle(!toggle); 
+                    setActive(link.title);
+                  }}
+                >
+                  <Link to={`#${link.id}`}>{link.title}</Link>
+                </li>
+              ))}
+              <li
+                onClick={() => {
+                  window.open(
+                    "https://drive.google.com/file/d/1HAnzHmD9-tC7LUO_SWOalmwINAWB0Bbi/view?usp=sharing"
+                  );
+                }}
+                className="border-amber-500 border-2 bg-violet-500 h-8 justify-center w-20 items-center flex rounded-xl font-bold cursor-pointer"
+              >
+                {" "}
+                Resume
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
